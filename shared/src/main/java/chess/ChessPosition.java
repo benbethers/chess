3a219239fd707a6c5736f2fab60e191e44e31e93
benbethers <benbethers@gmail.com>
2026-09-18 11:49:13 -0600

    finish class functions for ChessPosition function

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -20,7 +20,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return this.col;
     }
 
     /**
@@ -28,6 +28,6 @@ public class ChessPosition {
      * 1 codes for the left column
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return this.col;
     }
 }
